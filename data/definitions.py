14631f84b2b859130d0c7f370901e8a6a7915d2d
Anna Soboleva <asoboleva.avs@gmail.com>
2024-05-05 20:21:43 +0300

    main.py and definitions.py def user_interactions() updated

--- data/definitions.py
+++ data/definitions.py
@@ -1,15 +1,22 @@
-DATA_PATH = "./data/products.json"
+import os
+
+DATA_PATH = os.path.abspath('data')
+file_worker_hh = os.path.join(DATA_PATH, 'file_worker_hh.json')
+file_json = os.path.join(DATA_PATH, 'vacancies.json')
+file_txt = os.path.join(DATA_PATH, 'vacancies.txt')
+file_csv = os.path.join(DATA_PATH, 'vacancies.csv')
+user_choice_default = {'region': 'Москва', 'vacancy': 'Python'}
 strings_for_print = {
     "to_start": "Добро пожаловать на наш сервис!\nБыстро и удобно поможем найти лучшую для Вас работу.",
     "request_site": "Выберете сайт для поиска вакансии (введите 1, 2 или 3):\n1. hh.ru\n2. rabota.ru\n3. avito.ru",
     "request_region": "Введите регион: (пример: Санкт-Петербург, Москва, Саратовская область,...)",
     "request_vacancy": "Введите интересующую Вас вакансию: (пример: менеджер по продажам)",
-    "request_salary": "Введите ожидаемую зарплату - от: (пример: 50000)",
+    "request_salary": "Введите ожидаемую зарплату - от: (пример: 50000), либо 0, если зарплата может быть любой",
     "request_waiting": "Ваш запрос обрабатывается",
     "execution_phase": "Введите 0, 1, 2 или 3:\n0: закончить программу\n1: отправить запрос\n2: добавить результат "
                        "запроса в файл\n3: удалить вакансию из файла""",
     "choice": "Ваш выбор: ",
     "error_input": "Некорректный ввод, попробуйте еще раз",
-    "add_vacancy": "add_vacancy",
-    "del_vacancy": "del_vacancy"
-}
+    "add_vacancy": "Выберете формат файла, в котором необходимо сохранить данные:\n1. .json\n2. .txt\n3. .csv",
+    "del_vacancy": "Введите id вакансии, которую необходимо удалить: "
+    }
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import json
 from src.parser import HH
-from data.definitions import strings_for_print
-from src.my_exeption import MyException
+from data.definitions import strings_for_print, file_worker_hh, user_choice_default
+from src.my_exeption import RequestErrorException
+from src.vacancies import Vacancy
 
 
-def int_input(prompt, range_):
+def load_data(file_json):
+    """
+    Загружает данные из файла json
+    """
+    if file_json != '':
+        with open(file_json, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+
+
+def int_input_in_range(prompt, range_=None):
     input_int = 0
     ok = False
     while not ok:
@@ -13,24 +24,14 @@ def int_input(prompt, range_):
         except ValueError:
             print(strings_for_print["error_input"])
         else:
-            if input_int in range(range_[0], range_[1]+1):
-                ok = True
+            if range_:
+                if input_int in range(range_[0], range_[1] + 1):
+                    ok = True
+                else:
+                    print(strings_for_print["error_input"])
             else:
-                print(strings_for_print["error_input"])
+                ok = True
     return input_int
-
-
-def float_input(prompt):
-    input_float = 0
-    ok = False
-    while not ok:
-        try:
-            input_float = float(input(prompt))
-        except ValueError:
-            print(strings_for_print["error_input"])
-        else:
-            ok = True
-    return input_float
 
 
 def user_interactions(start):
@@ -38,35 +39,38 @@ def user_interactions(start):
     if start:
         print(strings_for_print["to_start"])
     print(strings_for_print["execution_phase"])
-    user_answer = int_input(strings_for_print["choice"], [0, 3])
+    user_answer = int_input_in_range(strings_for_print["choice"], [0, 3])
     if user_answer == 0:
         return True, request
     elif user_answer == 1:
         print(strings_for_print["request_site"])
-        request_site = int_input(strings_for_print["choice"], [1, 3])
+        site = int_input_in_range(strings_for_print["choice"], [1, 3])
         print(strings_for_print["request_region"])
-        request_region = input(strings_for_print["choice"])
+        region = input(strings_for_print["choice"])
+        if region == '':
+            region = user_choice_default['region']
         print(strings_for_print["request_vacancy"])
-        request_vacancy = input(strings_for_print["choice"])
+        vacancy = input(strings_for_print["choice"])
+        if vacancy == '':
+            vacancy = user_choice_default['vacancy']
         print(strings_for_print["request_salary"])
-        request_salary = float_input(strings_for_print["choice"])
+        salary_from = int_input_in_range(strings_for_print["choice"])
         print(strings_for_print["request_waiting"])
-        request = ['request', request_site, request_region, request_vacancy, request_salary]
+        # if salary_from == 0:
+        #     salary_from = 1
+        request = {'name': 'request', 'site': site, 'region': region, 'vacancy': vacancy,
+                   'salary_from': salary_from}
         return False, request
 
     elif user_answer == 2:
         print(strings_for_print["add_vacancy"])
-        add_vacancy = str(input(strings_for_print["choice"]))
-        request = ['add', add_vacancy]
+        add_vacancy = input(strings_for_print["choice"])
+        request = {'name': 'add'}
         return False, request
     elif user_answer == 3:
         print(strings_for_print["del_vacancy"])
-        del_vacancy = str(input(strings_for_print["choice"]))
-        request = ['del', del_vacancy]
-        return False, request
-    else:
-        print(strings_for_print["error_input"])
-        request = []
+        del_vacancy = input(strings_for_print["choice"])
+        request = {'name': 'del'}
         return False, request
 
 
@@ -74,11 +78,37 @@ def main():
     end = False
     start = True
     while not end:
-        end, request = user_interactions(start)
+        end, user_request = user_interactions(start)
         start = False
-        if request:
-            print(request)
-            data = []
+        if user_request:
+            if user_request['name'] == 'request':
+                if user_request['site'] == 1:
+                    parser_hh = HH(file_worker_hh)
+                    try:
+                        parser_hh.load_vacancies(user_request)
+                    except RequestErrorException as e:
+                        print('RequestError ', e, '\n')
+                    else:
+                        parser_hh.save_data_in_file(file_worker_hh)
+
+                if user_request['site'] == 2:
+                    print('Function is not implemented')
+                if user_request['site'] == 3:
+                    print('Function is not implemented')
+            elif user_request['name'] == 'add':
+                vacancies = []
+                print(user_request)
+                if file_worker_hh != '':
+                    data = load_data(file_worker_hh)
+                    for item in data:
+                        vacancy = Vacancy(item['name'], item['department'], item['salary'], item['snippet'],
+                                          item['schedule'], item['professional_roles'], item['experience'])
+
+                        vacancies.append(vacancy)
+                        print(vacancy.name, vacancy.salary)
+
+            elif user_request['name'] == 'del':
+                print(user_request)
 
 
 if __name__ == '__main__':
